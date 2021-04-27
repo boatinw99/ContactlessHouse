590abdb168353759e9a416ab6cb7333204fdca06
Puttinart <puttinart09@gmail.com>
2021-04-27 20:08:13 +0700

    add: device pane

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Device from "./components/devicePane";
 
 function App() {
   return (
     <div>
       <Header />
+      <Device />
     </div>
   );
 }
