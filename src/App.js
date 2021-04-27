ccfb9ff40a7102703b31752d22362c8555b4c29f
boatinw99 <thanathatboat@hotmail.com>
2021-04-27 16:59:53 +0700

    add controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Controls from './components/Controls';
 
 function App() {
   return (
     <div>
       <Header />
+      <Controls />
     </div>
   );
 }
