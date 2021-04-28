7be43b77e53f5b95b8bf75e67992d94403926034
Puttinart <puttinart09@gmail.com>
2021-04-28 22:59:28 +0700

    finish devicepane UI

--- src/components/Device.js
+++ src/components/Device.js
@@ -2,15 +2,16 @@ import React, { useCallback, useState } from "react";
 import { List, ListItem, Box, Typography, Container } from "@material-ui/core";
 import RoundPaper from "./RoundPaper";
 import openedDoor from "./door-opened.png";
-import DoorSwitch from "./Switch";
+import { DoorSwitch, LightSwitch } from "./Switch";
+import lightbulb from "./lightbulb.png";
 const Device = () => {
   const [checked, setChecked] = React.useState(true);
   return (
     <Box mt={2} mb={4} width="100%" alignContent="center">
       <RoundPaper
         style={{
-          width: 600,
-          height: 400,
+          width: 400,
+          height: 300,
           backgroundColor: "#f8bbd0",
         }}
       >
@@ -19,9 +20,17 @@ const Device = () => {
           src={openedDoor}
           width="80"
           height="80"
-          style={{ marginLeft: 100, marginTop: 40 }}
+          style={{ left: 60, top: 20, position: "relative" }}
         ></img>
         <DoorSwitch></DoorSwitch>
+        <img
+          id="lightbulb"
+          src={lightbulb}
+          width="80"
+          height="80"
+          style={{ right: 65, top: 200, position: "relative" }}
+        ></img>
+        <LightSwitch></LightSwitch>
       </RoundPaper>
     </Box>
   );
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import { Switch } from "antd";
 const DoorSwitch = () => {
+  let locked = false;
   return (
     <Switch
-      style={{ marginLeft: 150, marginTop: 40 }}
-      defaultChecked={false}
+      defaultChecked={locked}
+      onChange={(locked) => {
+        locked = !locked;
+      }}
+      style={{ left: 200, top: 30 }}
     ></Switch>
   );
 };
-export default DoorSwitch;
+const LightSwitch = () => {
+  let turnOn = false;
+  return (
+    <Switch
+      defaultChecked={turnOn}
+      onChange={(turnOn) => {
+        turnOn = !turnOn;
+      }}
+      style={{ left: 75, top: 200 }}
+    ></Switch>
+  );
+};
+export { DoorSwitch };
+export { LightSwitch };
