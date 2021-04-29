3f7b4826bf2431fcf50ff8dce7d918f22609e916
boatinw99 <thanathatboat@hotmail.com>
2021-04-29 18:45:48 +0700

    try adding firestore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
-import Controls from "./components/Controls";
 import History from "./components/History"
-// import SplitPane, { Pane } from "react-split-pane";
 import Device from "./components/Device";
+import firebase from 'firebase' ;
 
 function App() {
+  // const firebase = require("firebase");
+// Required for side-effects
+  require("firebase/firestore");
+   const firebaseConfig = {
+      apiKey: "AIzaSyASlgzTKt0fmOIUeCHHMLd3KUNacJOOAS8",
+      authDomain: "contactlesshouse-9cf98.firebaseapp.com",
+      projectId: "contactlesshouse-9cf98"
+      // databaseURL: "https://embededproject-93df5.firebaseio.com",
+      // storageBucket: "embededproject-93df5.appspot.com",
+      // messagingSenderId: "863008823567"
+    };
+  firebase.initializeApp(firebaseConfig);
+  // let db = firebase.firestore();
   return (
     <div className="App">
       <Header />
